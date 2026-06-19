52e0729a37dbd99d098f957364fc73b0a2d01730
MarcoNoLimits <ahmed.teca.kanadji911@gmail.com>
2026-06-19 04:02:10 +0300

    Update main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './components/styles/colors.css';
+import './components/Styles/colors.css';
 import App from './App.tsx'
 
 
